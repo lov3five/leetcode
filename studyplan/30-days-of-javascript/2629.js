4a2385c7700a630201a86e75be7120a850137296
lov3five <luongtranstaff@gmail.com>
2024-03-26 11:16:49 +0700

    update

---
+++ studyplan/30-days-of-javascript/2629.js
@@ -0,0 +1,41 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+
+    return function (x) {
+        functions = functions.reverse()
+        return functions.reduce((accumX, currentFunction) => {
+            return currentFunction(accumX)
+        }, x)
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
+
+////////////////
+// Method 2
+
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+
+    return function (x) {
+        let len = functions.length
+        for (let i = len - 1; i >= 0; i--){
+            x = functions[i](x)
+        }
+        return x
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
