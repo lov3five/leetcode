86d6d5f718174314e951839c75a642505e86a6d8
lov3five <luongtranstaff@gmail.com>
2024-04-03 15:54:25 +0700

    Problems: reverse integer

---
+++ problems/7/reverse-integer.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let res = 0;
+    if (x < 0) {
+        res = parseInt(String(x).slice(1).split('').reverse().join('')) * -1;
+    } else {
+        res = parseInt(String(x).split('').reverse().join(''));
+    }
+
+    if (res > Math.pow(2, 31) - 1 || res < -Math.pow(2, 31)) {
+        return 0;
+    }
+
+    return res;    
+};
