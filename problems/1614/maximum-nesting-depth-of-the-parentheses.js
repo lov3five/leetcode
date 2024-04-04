c81215d0cf15f67ca3684722735811a82b53439f
lov3five <luongtranstaff@gmail.com>
2024-04-04 12:52:29 +0700

    Resolve 1614 daily challange

---
+++ problems/1614/maximum-nesting-depth-of-the-parentheses.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+    let maxDepth = 0, depth = 0
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] == "(") {
+            depth++
+        } 
+        if(s[i] == ")") {
+            depth--
+        }
+        if(depth > maxDepth) {
+            maxDepth = depth
+        }
+    }
+    return maxDepth
+};
