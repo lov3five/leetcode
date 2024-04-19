f80fb46b90f77df2abaa440a9a40ab6f4c852221
lov3five <luongtranstaff@gmail.com>
2024-04-19 11:09:36 +0700

    Hot fix

--- studyplan/30-days-of-javascript/reduce_custom.js
+++ studyplan/30-days-of-javascript/reduce_custom.js
@@ -1,13 +1,16 @@
 const reduce = (arr, callback, initValue) => {
-    let acc = initValue === undefined ? arr[0] : initValue
-    for(let i = 0; i < arr.length; i++) {
+    let acc = (initValue === undefined ? arr[0] : initValue);
+
+    let startIndex = (initValue === undefined ? 1 : 0);
+    
+    for (let i = startIndex; i < arr.length; i++) {
         acc = callback(acc, arr[i], i, arr)
     }
     return acc
 }
 
 // Test
-const array1 = [1,2];
+const array1 = [1, 2];
 
 // 0 + 1 + 2 + 3 + 4
 const initialValue = 5;
